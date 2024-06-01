430512592df06b084c5fc6680d5028a118ca45c0
mratwong <shinminkha253@gmail.com>
2024-06-01 09:16:39 +0630

    Sql Injection

---
+++ MWDotNetCore.WinFormsAppSqlInjection/ConnectionStrings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace MWDotNetCore.WinFormsAppSqlInjection
+{
+    internal static class ConnectionStrings
+    {
+        public static SqlConnectionStringBuilder SqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = ".",
+            InitialCatalog = "DotNetTrainingBatch4",
+            UserID = "sa",
+            Password = "sasa@123",
+            TrustServerCertificate = true
+        };
+    }
+}
---
+++ MWDotNetCore.WinFormsAppSqlInjection/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace MWDotNetCore.WinFormsAppSqlInjection
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtEmail = new TextBox();
+            btnLogin = new Button();
+            txtPassword = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(294, 114);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(340, 31);
+            txtEmail.TabIndex = 0;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.RoyalBlue;
+            btnLogin.ForeColor = Color.White;
+            btnLogin.Location = new Point(294, 255);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(112, 46);
+            btnLogin.TabIndex = 1;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = false;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(294, 202);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(340, 31);
+            txtPassword.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(294, 66);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 25);
+            label1.TabIndex = 3;
+            label1.Text = "Email";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(294, 159);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 25);
+            label2.TabIndex = 4;
+            label2.Text = "Password";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(940, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtPassword);
+            Controls.Add(btnLogin);
+            Controls.Add(txtEmail);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtEmail;
+        private Button btnLogin;
+        private TextBox txtPassword;
+        private Label label1;
+        private Label label2;
+    }
+}
